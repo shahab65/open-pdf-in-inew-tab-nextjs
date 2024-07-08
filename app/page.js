5bf6e14c9658be654b4e57f81a74ef8482b9c529
shahab <sh.emami@nizek.com>
2024-07-09 00:58:06 +0330

    edit text

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <a href={pdfUrl} target="_blank" rel="noopener noreferrer">
-        open pdf directly
+        open pdf in new tab
       </a>
     </main>
   );
